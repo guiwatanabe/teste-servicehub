79de1b56b164978d2dd4f5e1cdc55b6c8bd9881c
Guilherme Watanabe <guiwatanabe@hotmail.com>
2026-02-01 22:21:52 -0300

    chore: adding axios

---
+++ resources/js/services/http.ts
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+axios.defaults.withCredentials = true;
+axios.defaults.withXSRFToken = true;
+
+const http = axios.create({
+    baseURL: '/api',
+    headers: {
+        Accept: 'application/json',
+    },
+})
+
+axios.get('/sanctum/csrf-cookie').catch(() => { })
+
+export default http
